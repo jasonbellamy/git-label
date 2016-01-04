d2e43e8bfe1ec0b6ec67db3a7553ff1673816c84
Jason Bellamy <j@sonbellamy.com>
2016-01-04 18:58:07 -0500

    Add default api url (https://api.github.com)

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,15 +4,15 @@
  * @name configure
  * @function
  * @param {Object} server the server configuration object
- * @param {String} server.api the api endpoint to connect to
+ * @param {String} [server.api=https://api.github.com] the api endpoint to connect to
  * @param {String} server.token the api token to use
  * @param {String} server.repo the git repo to manipulate
  * @return {Object} structured server configuration object
  */
 export function configure({api, token, repo}) {
   return {
-    api,
-    token,
-    repo: `repos/${repo}`
+    api: api || 'https://api.github.com',
+    repo: `repos/${repo}`,
+    token
   };
 }
